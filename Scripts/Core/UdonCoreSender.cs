aefc09e5be46bedef2101f567db2f38bea2ad258
Sonic853 <Sonic853@qq.com>
2025-08-02 21:55:04 +0800

    位置调整

--- Scripts/Core/UdonCoreSender.cs
+++ Scripts/Core/UdonCoreSender.cs
@@ -10,15 +10,15 @@ namespace Sonic853.Udon.SendFunction
     public class UdonCoreSender : UdonSharpBehaviour
     {
         /// <summary>
-        /// 需要调用的 Udon Sender
-        /// </summary>
-        [Header("需要调用的 Udon Sender")]
-        public UdonBehaviour[] udonSender;
-        /// <summary>
         /// 只允许触发一次
         /// </summary>
         [Header("只允许触发一次")]
         public bool isOnce = false;
+        /// <summary>
+        /// 需要调用的 Udon Sender
+        /// </summary>
+        [Header("需要调用的 Udon Sender")]
+        public UdonBehaviour[] udonSender;
         /// <summary>
         /// 是否已触发
         /// </summary>
